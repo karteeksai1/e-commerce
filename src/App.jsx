99dd475b22a780accf7d80125b9bc9bd47c372a4
karteek <karteek0011@gmail.com>
2026-03-06 19:11:51 +0530

    created multiple pages using routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import { useState } from 'react'
+import { Routes ,Route} from 'react-router'
 import { HomePage } from './pages/HomePage'
 import './App.css'
 
 function App() {
   return (
     <>
-      <HomePage></HomePage>
+    <Routes>
+      <Route path="/" element={<HomePage></HomePage>}></Route>
+      <Route path='checkout' element={<div> Test</div>}></Route>
+
+    </Routes>
+      
     </>
   )
 }
