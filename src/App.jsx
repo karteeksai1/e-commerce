561aa2d51ce6166467720d932bba5cd3eb5edc9d
karteek <karteek0011@gmail.com>
2026-03-06 19:39:42 +0530

    added routes for tracking and orders

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes ,Route} from 'react-router'
 import { HomePage } from './pages/HomePage'
 import {CheckoutPage} from './pages/CheckoutPage'
+import { OrdersPage } from './pages/OrdersPage'
+import {Tracking} from './pages/Tracking'
 import './App.css'
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
     <Routes>
       <Route path="/" element={<HomePage></HomePage>}></Route>
       <Route path='checkout' element={<CheckoutPage/>}></Route>
+      <Route path='orders' element={<OrdersPage/>}/>
+      <Route path='tracking' element={<Tracking/>}/>
 
     </Routes>
       
