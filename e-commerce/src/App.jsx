1b1f91eaabfdc9403cc48bf7fc539506923e5100
karteek <karteek0011@gmail.com>
2026-03-11 12:58:34 +0530

    set up backend for orders checkout pages

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -1,34 +1,29 @@
-import { Routes ,Route} from 'react-router'
-import { HomePage } from './pages/HomePage'
-import {CheckoutPage} from './pages/CheckoutPage'
-import { OrdersPage } from './pages/OrdersPage'
-import {Tracking} from './pages/Tracking'
+import axios from 'axios';
+import { Routes, Route } from 'react-router';
+import { useState, useEffect } from 'react';
+import { HomePage } from './pages/home/HomePage';
+import { CheckoutPage } from './pages/checkout/CheckoutPage';
+import { OrdersPage } from './pages/orders/OrdersPage';
 import './App.css'
-import { useState,useEffect } from 'react'
-import axios from 'axios'
-
 
 function App() {
-  const [cart,setCart]=useState([])
-  useEffect(()=>{
-axios.get('http://localhost:3000/api/cart-items?expand=product')
-    .then((response)=>{
-      setCart(response.data)
-    })
-  },[])
-  
-  
-  return (
-    <>
-    <Routes>
-      <Route path="/" element={<HomePage cart={cart}></HomePage>}></Route>
-      <Route path='checkout' element={<CheckoutPage cart={cart}/>}></Route>
-      <Route path='orders' element={<OrdersPage/>}/>
-      <Route path='tracking' element={<Tracking/>}/>
+  const [cart, setCart] = useState([]);
 
+  useEffect(() => {
+    const fetchAppData = async () => {
+      const response = await axios.get('/api/cart-items?expand=product');
+      setCart(response.data);
+    };
+
+    fetchAppData();
+  }, []);
+
+  return (
+    <Routes>
+      <Route index element={<HomePage cart={cart} />} />
+      <Route path="checkout" element={<CheckoutPage cart={cart} />} />
+      <Route path="orders" element={<OrdersPage cart={cart} />} />
     </Routes>
-      
-    </>
   )
 }
 
